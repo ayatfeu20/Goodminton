77a4dd90448dd44676f6f6ea23e7fcd56a45b51e
ayatfeu20 <ayat_manaa@yahoo.com>
2022-04-08 11:01:44 +0200

    new add

--- src/pages/Kunskaper/Kunskaper.js
+++ src/pages/Kunskaper/Kunskaper.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import background from './/learning-g64aed81a6_1280.jpg';
+import  "./Kunskaper.css";
 
 function Kunskaper() {
   return (
-    <div>Kunskaper</div>
+    <section id="kunskaper" class="kunskaper">
+      <div className="d-flex ">
+     <div className="background" style={{ backgroundImage:`url(${background})`,backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition: 'center', height: '60vh',width:'100%'}}>
+     <h2 className='d-flex'>Kunskaper</h2>
+     </div>
+     </div>
+
+     <div class="container" data-aos="fade-up">
+      <div className='kun'>
+         
+        <h2 className='d-flex' >Våra konsulter har hög generell kompetens </h2>
+        <p>Här följer exempel på områden där vi håller absolut toppklass.  </p>
+      </div>
+      </div>
+
+     </section>
   )
 }
 
--- src/pages/Projekt/Projekt.js
+++ src/pages/Projekt/Projekt.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import background from './/puzzle-g1b4841205_1920.jpg';
+import  "./Projekt.css";
 
 function Projekt() {
   return (
-    <div>Projekt</div>
+    <section id="projekt" class="projekt">
+      <div className="d-flex ">
+     <div className="background" style={{ backgroundImage:`url(${background})`,backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition: '62% 38%', height: '60vh',width:'100%'}}>
+     <h2 className='d-flex'>Uppdrag och projekt</h2>
+     </div>
+     </div>
+
+     <div class="container" data-aos="fade-up">
+      <div className='av'>
+         
+        <h2 className='d-flex' >Kunskaperna ger oss fördelar och detta tar vi med oss till våra kunder </h2>
+        <p>Våra interna projekt drivs affärsmässigt och ger våra konsulter möjlighet att testa och tillämpa den senaste tekniken likväl som att laborera med affärsmodeller. </p>
+      </div>
+      </div>
+
+     </section>
   )
 }
 
